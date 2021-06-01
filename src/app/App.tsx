b4a092fd2c423f4ba596dd772992b64ce4908f87
rabram1 <romanabram@web.de>
2021-06-01 08:29:11 +0200

    Add more routes to App()

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,36 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Login, Register } from './components/Button/Button.stories';
+import Cast from './pages/Cast/Cast';
+import ForgotPassword from './pages/ForgotPassword/ForgotPassword';
+import Homescreen from './pages/Homescreen/Homescreen';
 import MovieDetails from './pages/MovieDetails/MovieDetails';
+import TVShowDetails from './pages/TVShowDetails/TVShowDetails';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/Home">
-          <p>Home</p>
+        <Route exact path="/">
+          <Homescreen />
         </Route>
         <Route exact path="/movie/:name">
           <MovieDetails />
+        </Route>
+        <Route exact path="/cast/:name">
+          <Cast />
+        </Route>
+        <Route exact path="/forgotpassword">
+          <ForgotPassword />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/show/:name">
+          <TVShowDetails />
+        </Route>
+        <Route exact path="register">
+          <Register />
         </Route>
       </Switch>
     </BrowserRouter>
